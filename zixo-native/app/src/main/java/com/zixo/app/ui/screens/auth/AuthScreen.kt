ccf0dca9fc77dd4eff6ecb5706464529d00b0347
Z User <z@container>
2026-06-15 18:47:32 +0000

    a5b5fc5e-5b6c-4571-8cef-7a87cd6a8d6d

--- zixo-native/app/src/main/java/com/zixo/app/ui/screens/auth/AuthScreen.kt
+++ zixo-native/app/src/main/java/com/zixo/app/ui/screens/auth/AuthScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
